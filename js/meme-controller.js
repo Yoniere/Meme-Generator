c8507097c44e236efdc88262e26148b75681614d
Yonatan Erez <jontaner12@gmail.com>
2022-02-16 16:34:57 +0200

    2nd version

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -3,21 +3,42 @@
 var gCanvas;
 var gCtx;
 
+// function init() {
+//     elMemeGallery = document.querySelector('.meme-gallery')
+//     elMemeGallery.style.display = 'block'
+// }
+renderMeme()
 
-function init() {
-    const elMemeGallery = document.querySelector('.meme-gallery')
-    elMemeGallery.style.display = 'block'
-}
-
-function renderMeme() {
-    elMemeGallery.style.display = 'none'
+function renderMeme(txt) {
+    var meme = getMeme()
     gCanvas = document.getElementById('my-canvas');
     gCtx = gCanvas.getContext('2d');
-    const elMemeEditor = document.querySelector('.meme-editor')
-    elMemeGallery.style.display = 'block'
-
+    drawImg()
+    drawText(txt, 300, 100)
 }
 
-function pickImageForCanvas(this) {
-    console.log('hello')
+function onSetLineTxt() {
+    var elTxt = document.querySelector('input').value;
+    setLineTxt(elTxt)
+    document.querySelector('input').value = '';
+    renderMeme(elTxt)
+}
+
+function drawImg() {
+    var elImg = document.querySelector('img');
+    // Naive approach:
+    // there is a risk that image is not loaded yet and nothing will be drawn on canvas
+    gCtx.drawImage(elImg, 0, 0, gCanvas.width, gCanvas.height);
+}
+
+function drawText(text, x, y) {
+    // gCtx.font = '48px serif';
+    // gCtx.fillText(text, x, y);
+
+    gCtx.lineWidth = 1;
+    gCtx.strokeStyle = 'brown';
+    gCtx.fillStyle = 'black';
+    gCtx.font = '20px Arial';
+    gCtx.fillText(text, x, y);
+    gCtx.strokeText(text, x, y);
 }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,33 @@
 'use strict'
+
+// var gImgs = [{
+//         id: '1',
+//         url: "img/1.jpg",
+//         keywords: []
+//     },
+//     {
+//         id: '2',
+//         url: "img/2.jpg",
+//         keywords: []
+//     }
+// ];
+
+var gMeme = {
+    selectedImgId: '',
+    selectedLineIdx: '',
+    lines: [{
+        txt: '',
+        size: 0,
+        align: '',
+        color: ''
+    }]
+}
+
+function getMeme() {
+    return gMeme;
+}
+
+function setLineTxt(txt) {
+    gMeme.lines[0].txt = txt;
+    console.log(gMeme)
+}
