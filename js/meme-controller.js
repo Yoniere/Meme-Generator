977a8e45227c4dc1d3efdca077df021c03c8a2da
Yonatan Erez <jontaner12@gmail.com>
2022-02-16 12:10:06 +0200

    first version

---
+++ js/meme-controller.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var gCanvas;
+var gCtx;
+
+
+function init() {
+    const elMemeGallery = document.querySelector('.meme-gallery')
+    elMemeGallery.style.display = 'block'
+}
+
+function renderMeme() {
+    elMemeGallery.style.display = 'none'
+    gCanvas = document.getElementById('my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    const elMemeEditor = document.querySelector('.meme-editor')
+    elMemeGallery.style.display = 'block'
+
+}
+
+function pickImageForCanvas(ev) {
+    console.log(ev.src)
+}
